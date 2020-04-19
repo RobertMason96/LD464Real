32201b0dd54cb3476911d3b3cbaf2aeeda6396dd
RobertMason96 <robert.mason343@gmail.com>
2020-04-19 14:39:54 +0100

    Fixed Jumping Can Pickup
    Fixed Jumping Can Pickup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,10 +16,22 @@ public class PlayerController : MonoBehaviour
     float jumpforce = 5f;
     public float minv = 1f;
     public float friction = 0f;
+    public float airFriction = 5f;
+
+    public GameObject holderObject = null;
+    public float jumpHeight =1f;
+    public Transform holderempty;
     // Start is called before the first frame update
     void Start()
     {
         controller =  this.GetComponent<CharacterController>();
+        foreach(Transform t in this.GetComponentsInChildren<Transform>())
+        {
+            if(t.transform.gameObject.name=="HolderEmpty")
+            {
+                holderempty = t;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -42,13 +54,86 @@ public class PlayerController : MonoBehaviour
         {
             inplaneInput.y--;
         }
-        if (!controller.isGrounded)
+        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 100f, Color.yellow);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            Debug.Log("DOWN");
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            layerMask = ~layerMask;
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * jumpHeight, Color.yellow);
+
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out RaycastHit hit,jumpHeight, layerMask))
             {
-                jump = true;
-            }            
+                jump = true;                
+                Debug.Log("Did Hit");
+            }
+            
+            
+        }            
+        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(holderObject==null)
+            {
+                // Bit shift the index of the layer (8) to get a bit mask
+                int layerMask = 1 << 8;
+
+                // This would cast rays only against colliders in layer 8.
+                // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+                layerMask = ~layerMask;
+
+
+                // Does the ray intersect any objects excluding the player layer
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, Mathf.Infinity, layerMask))
+                {
+                    if (hit.transform.tag == "Pickup")
+                    {
+                        holderObject = hit.transform.gameObject;
+                        Debug.Log("Did Pickup Hit");
+                        if(holderObject.GetComponent<Rigidbody>()!=null)
+                        {
+                            holderObject.GetComponent<Rigidbody>().isKinematic = true;
+                        }
+                        if (holderObject.GetComponent<Collider>() != null)
+                        {
+                            holderObject.GetComponent<Collider>().enabled = false;
+                        }
+                    }
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    Debug.Log("Did Hit");
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                    Debug.Log("Did not Hit");
+                }
+
+            }
+            else
+            {
+                if (holderObject.GetComponent<Rigidbody>() != null)
+                {
+                    holderObject.GetComponent<Rigidbody>().isKinematic = false;
+                }
+                if (holderObject.GetComponent<Collider>() != null)
+                {
+                    holderObject.GetComponent<Collider>().enabled = true;
+                }
+                holderObject = null;
+            }
         }
+
+        if(holderObject!=null)
+        {
+            holderObject.transform.position = holderempty.position;
+            holderObject.transform.rotation = holderempty.rotation;
+        }
+        
     }
     public void FixedUpdate()
     {
@@ -59,7 +144,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            friction = 0f;
+            friction = airFriction;
         }
 
         float movey=velocity.y;
@@ -72,6 +157,7 @@ public class PlayerController : MonoBehaviour
             if(jump)
             {
                 movey += jumpforce;
+                jump = false;
             }
             else
             {
@@ -93,12 +179,20 @@ public class PlayerController : MonoBehaviour
 
         velocity = new Vector3(inplaneVelocity.x, movey, inplaneVelocity.y);
 
+        if(inplaneVelocity.magnitude!=0)
+        {
+            transform.forward = new Vector3(inplaneVelocity.x,0,inplaneVelocity.y);
+        }
+        
+
+
+
         controller.Move(velocity * Time.fixedDeltaTime);
         
         //Add in velocity
 
 
         
-        jump = false;
+        
     }
 }
