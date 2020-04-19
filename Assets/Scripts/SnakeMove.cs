52535b3e50371fbbe01a14d6f73a8d01715f23af
David Mason <davemason232@gmail.com>
2020-04-19 22:46:01 +0100

    Improved Snake AI
    Improved Snake AI
    Added food drop

--- Assets/Scripts/SnakeMove.cs
+++ Assets/Scripts/SnakeMove.cs
@@ -9,12 +9,10 @@ public class SnakeMove : MonoBehaviour
     public float moveSpeed;
     private float timer;
 
-    //Waypoint information
-    public GameObject Waypoint;
-    private bool IsFood; //is the next waypoint food?
-
     //Spacing of the grid in the world
     public float gridSize;
+
+    private Vector3 movementDirection;
 
     //Food
     private GameObject food;
@@ -22,14 +20,14 @@ public class SnakeMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        IsFood = true;
         DeBody = new List<GameObject>();
         DeBody.Add(GameObject.FindGameObjectWithTag("SnakeHead"));
         DeBody.Add(GameObject.FindGameObjectWithTag("SnakePart"));
         DeBody.Add(GameObject.FindGameObjectWithTag("SnakeTail"));
+        movementDirection = -DeBody[0].transform.right;
         timer = 0;
         food = findFood();
-        FindNextWayPoint();
+        getDirections();
     }
 
     // Update is called once per frame
@@ -37,97 +35,136 @@ public class SnakeMove : MonoBehaviour
     {
         if(timer >= (2 / moveSpeed))
         {
-            findFood();
-            FindNextWayPoint();
-            if (findDirection(DeBody[0]).magnitude <= gridSize)
-            {
-                FindNextWayPoint();
-                if (IsFood)
-                {
-                    foodEatten();
-                }
-                else
-                {
-                    moveSnake();
-                }
-            }
-            else
-            {
-                moveSnake();
-            }
+            food = findFood();
+            getDirections();
+            moveSnake();
             timer = 0;
         }
         timer += Time.deltaTime;
     }
-    private Vector3 findDirection(GameObject gObj)
+    private void getDirections()
     {
-        Vector3 dir = new Vector3(Waypoint.transform.position.x - gObj.transform.position.x, 0 , Waypoint.transform.position.z- gObj.transform.position.z);
-        return dir;
-        
-    }
-    private void FindNextWayPoint()
-    {
-        if((Mathf.Abs(food.transform.position.x - DeBody[0].transform.position.x) >= gridSize) && (Mathf.Abs(food.transform.position.z - DeBody[0].transform.position.z) >= gridSize))
+        if (food != null)
         {
-            Debug.Log("poop here");
-            IsFood = false;
-            Waypoint.transform.position = new Vector3(DeBody[0].transform.position.x, DeBody[0].transform.position.y, Mathf.Floor(food.transform.position.z / gridSize) * gridSize);
+            Vector3 moveDir = new Vector3(0, 0, 0);
+            moveDir = new Vector3(food.transform.position.x - DeBody[0].transform.position.x, DeBody[0].transform.position.y, food.transform.position.z - DeBody[0].transform.position.z);
+
+            if (Mathf.Abs(moveDir.x) < gridSize && Mathf.Abs(moveDir.z) < gridSize)
+            {
+                foodEatten(food);
+                Debug.Log(movementDirection);
+            }
+            else
+            {
+                if (Mathf.Abs(movementDirection.x) > Mathf.Abs(movementDirection.z))
+                {
+                    if (Mathf.Abs(moveDir.x) <= gridSize)
+                    {
+                        movementDirection = Mathf.Sign(moveDir.z) * new Vector3(0, 0, 1) * gridSize;
+                    }
+                    else
+                    {
+                        if (Mathf.Sign(moveDir.x) != Mathf.Sign(movementDirection.x))
+                        {
+                            movementDirection = Mathf.Sign(moveDir.z) * new Vector3(0, 0, 1) * gridSize;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Mathf.Abs(moveDir.z) <= gridSize)
+                    {
+                        movementDirection = Mathf.Sign(moveDir.x) * new Vector3(1, 0, 0) * gridSize;
+                    }
+                    else
+                    {
+                        if (Mathf.Sign(moveDir.z) != Mathf.Sign(movementDirection.z))
+                        {
+                            movementDirection = Mathf.Sign(moveDir.x) * new Vector3(1, 0, 0) * gridSize;
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    private Vector3 getAngle(Vector3 direction)
+    {
+        Vector3 temp = new Vector3(0,0,0);
+        if(direction.x == 0)
+        {
+            if (direction.z >0)
+            {
+                temp = new Vector3(0, 90, 0);
+            }
+            else
+            {
+                temp = new Vector3(0, -90, 0);
+            }
         }
         else
         {
-            IsFood = true;
-            Waypoint.transform.position = new Vector3(Mathf.Floor(food.transform.position.x / gridSize), DeBody[0].transform.position.y, Mathf.Floor(food.transform.position.z / gridSize)) * gridSize;
+            if (direction.x > 0)
+            {
+                temp = new Vector3(0, 180, 0);
+            }
+            else
+            {
+                temp = new Vector3(0, 0, 0);
+            }
         }
+        return temp;
     }
-    private void foodEatten()
+
+    private void foodEatten(GameObject temp)
     {
+        TileController tC = temp.transform.parent.gameObject.GetComponent<TileController>();
+        if (tC != null)
+        {
+            tC.createNewFood();
+        }
+        Destroy(temp.transform.gameObject);
+
         DeBody.Add(DeBody[DeBody.Count - 1]);
         DeBody[DeBody.Count - 2] = Instantiate(BodyPart, DeBody[DeBody.Count - 1].transform.position, DeBody[DeBody.Count - 1].transform.rotation);
-        DeBody[DeBody.Count - 2].transform.parent = this.transform;
-
-        Vector3 newPos = DeBody[0].transform.position;
-        Vector3 oldPos = DeBody[0].transform.position;
-
-        Vector3 moveDir = new Vector3(0, 0, 0);
-        moveDir = findDirection(DeBody[0]);
-        DeBody[0].transform.position += moveDir.normalized * gridSize;
-        DeBody[0].transform.rotation = Quaternion.Euler(0, Mathf.Sign(moveDir.x) * Mathf.Sign(moveDir.z) * (Vector3.Angle(moveDir, transform.forward)) + 90, 0);
-        for (int i = 1; i < DeBody.Count - 1; i++)
-        {
-            newPos = DeBody[i].transform.position;
-            moveDir = newPos - oldPos;
-            DeBody[i].transform.position = oldPos;
-            DeBody[i].transform.rotation = Quaternion.Euler(0, Mathf.Sign(moveDir.x) * Mathf.Sign(moveDir.z) * (Vector3.Angle(moveDir, transform.forward)) - 90, 0);
-            oldPos = newPos;
-        }
+        DeBody[DeBody.Count - 2].transform.parent = this.transform;      
 
     }
+
     private void moveSnake()
     {
         Vector3 newPos = DeBody[0].transform.position;
         Vector3 oldPos = DeBody[0].transform.position;
 
-        Vector3 moveDir = new Vector3(0, 0, 0);
-        moveDir = findDirection(DeBody[0]);
-        DeBody[0].transform.position += moveDir.normalized * gridSize;
-        DeBody[0].transform.rotation = Quaternion.Euler(0, Mathf.Sign(moveDir.x) * Mathf.Sign(moveDir.z) * (Vector3.Angle(moveDir, transform.forward)) + 90, 0);
+        Vector3 moveDir = movementDirection;
+
+        CheckAhead(moveDir);
+
+        DeBody[0].transform.position += moveDir;
+        DeBody[0].transform.rotation = Quaternion.Euler(getAngle(moveDir));
+             
         for (int i = 1; i < DeBody.Count; i++)
         {
             newPos = DeBody[i].transform.position;
             moveDir = newPos - oldPos;
             DeBody[i].transform.position = oldPos;
-            DeBody[i].transform.rotation = Quaternion.Euler(0, Mathf.Sign(moveDir.x) * Mathf.Sign(moveDir.z) * (Vector3.Angle(moveDir, transform.forward)) - 90, 0);
+            DeBody[i].transform.rotation = Quaternion.Euler(getAngle(moveDir));
             oldPos = newPos;
         }
     }
+
     private GameObject findFood()
     {
-        float shortestDistance = 100.0f;
+        
         int i = 0;
         int x = 0;
+        GameObject temp = null;
         GameObject[] F = GameObject.FindGameObjectsWithTag("Food");
-        if (F != null)
+        if (F.Length != 0)
         {
+            float shortestDistance = (DeBody[0].transform.position - F[0].transform.position).magnitude;
             foreach (GameObject gObj in F) 
             {
                 if (shortestDistance > (DeBody[0].transform.position - gObj.transform.position).magnitude)
@@ -137,7 +174,35 @@ public class SnakeMove : MonoBehaviour
                 }
                 x++;
             }
+            temp = F[i];
         }
-        return F[i];
+        return temp;
     }
+
+    private void CheckAhead(Vector3 direction)
+    {
+        int layerMask = ~(1 << 9);
+        RaycastHit hit;
+        if (Physics.Raycast(DeBody[0].transform.position , direction, out hit, 1.5f * gridSize, layerMask))
+        {
+            itHit(hit);
+        }else if(Physics.Raycast(DeBody[0].transform.position + (DeBody[0].transform.forward * (0.5f * gridSize)), direction, out hit, 1.5f * gridSize, layerMask))
+        {
+            itHit(hit);
+        }
+        else if (Physics.Raycast(DeBody[0].transform.position - (DeBody[0].transform.forward * (0.5f * gridSize)), direction, out hit, 1.5f * gridSize, layerMask))
+        {
+            itHit(hit);
+        }
+
+    }
+
+    private void itHit(RaycastHit hit)
+    {
+        if (hit.transform.tag == "SnakePart" || hit.transform.tag == "SnakeTail")
+        {
+            Debug.Log("Dead");
+        }
+    }
+
 }
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public GameObject food;
+    public float gridSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void createNewFood()
+    {
+        int maxGrid = 9;
+        int xPos = Random.Range(- maxGrid + 1, maxGrid);
+        int zPos = Random.Range(- maxGrid + 1, maxGrid);
+        GameObject temp = Instantiate(food,this.transform.position + new Vector3(xPos* gridSize, 10, zPos * gridSize), Quaternion.identity);
+        temp.transform.parent = this.transform;
+    }
+}
