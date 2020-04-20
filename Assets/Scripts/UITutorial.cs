71fd632d85f7dd15918441c9ae7582a9be69dbcd
David Mason <davemason232@gmail.com>
2020-04-21 00:27:00 +0100

    Tutorial Text at start of game
    Tutorial Text at start of game

---
+++ Assets/Scripts/UITutorial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UITutorial : MonoBehaviour
+{
+    private bool WASD;
+    private bool E;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WASD = false;
+        E = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!WASD)
+        {
+            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            {
+                WASD = true;
+                this.gameObject.GetComponent<Text>().text = "E to pick up food";
+            }
+        }
+        else
+        {
+            if (!E)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    E = true;
+                    Destroy(this.gameObject);
+                }
+            }
+
+        }
+    }
+}
